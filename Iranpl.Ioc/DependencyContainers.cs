f9ba5f883c52f86cf3546ffbe436aeb160bbb1b4
AalaThz <117166582+AalaThz@users.noreply.github.com>
2024-02-19 09:27:35 +0330

    upload orginal IranPl on Github

---
+++ Iranpl.Ioc/DependencyContainers.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Iranpl.ApplicationCore.Profiles;
+using Iranpl.ApplicationCore.Services.Implementations;
+using Iranpl.ApplicationCore.Services.Intefaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Iranpl.Ioc
+{
+    public static class DependencyContainers
+    {
+        public static void RegisterServices(this IServiceCollection service, IConfiguration configuration)
+        {
+            #region DI
+            service.AddScoped<ILoginService, LoginService>();
+            //service.AddScoped<IHttpService, HttpService>();
+            #endregion
+
+            #region AutoMapper
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            service.AddSingleton(mapper);
+            #endregion
+        }
+    }
+}
